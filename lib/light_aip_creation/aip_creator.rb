cf0dc1086f1529171a1c8bd787658723b30a9d05
astrilet <astrilet@gmail.com>
2017-11-01 14:50:10 -0600

    get create_aip working

--- lib/light_aip_creation/aip_creator.rb
+++ lib/light_aip_creation/aip_creator.rb
@@ -9,6 +9,7 @@ class LightAipCreation::AipCreator
   def initialize(workdir, noid = '')
     @work_dir = File.expand_path(workdir)
     @aip_directory="#{@work_dir}/#{noid}"
+    @aip_files = Array.new
   end
 
   def bag_tar_aip_dir(aip_directory)
@@ -18,8 +19,12 @@ class LightAipCreation::AipCreator
   end
 
   def create_aip(options = {})
-      assemble_aip(options)
-      bag_tar_aip_dir()
+    aip_files(options)
+    validate_files
+    assemble_aip
+    tar_aip_file = bag_tar_aip_dir(nil)
+    rm_aip_dir
+    tar_aip_file
   end
 
   def bag_aip
@@ -36,88 +41,98 @@ class LightAipCreation::AipCreator
     tar_aip_filename
   end
 
-  def assemble_aip(options)
-    validate_files
+  def assemble_aip
     make_aip_dirs
     copy_aip_files
-    rm_aip_dir
   end
 
   ### AIP Directories
   def aip_dirs
-      @aip_dirs = [
-        "#{@aip_directory}/data/objects",
-        "#{@aip_directory}/data/objects/metadata",
-        "#{@aip_directory}/data/logs",
-        "#{@aip_directory}/data/thumbnails"
-      ]
+    @aip_dirs = OpenStruct.new(
+      objects: "#{@aip_directory}/data/objects",
+      metadata: "#{@aip_directory}/data/objects/metadata",
+      logs: "#{@aip_directory}/data/logs",
+      thumbnails: "#{@aip_directory}/data/thumbnails"
+     )
   end
 
   def make_aip_dirs
-    aip_dirs.each { |path| FileUtils.mkdir_p(path) }
+    # FileUtils.mkdir_p(@aip_directory) unless File.exists?(@aip_directory)
+    aip_dirs.to_h.each_value { |path| FileUtils.mkdir_p(path) unless File.exists?(path) }
   end
 
   def rm_aip_dir
-    return unless File.exist?(@aip_directory)
-    FileUtils.rm_rf(@aip_directory)
+    FileUtils.rm_rf(@aip_directory) unless !File.exist?(@aip_directory)
   end
 
   ### AIP Files
-  def aip_files
-    @aip_files = [
-      # required files
-      # object_metadata
-      {
-        remote: @options[:object_metadata],
-        local: "#{aip_dirs.metadata}/object_metadata.n3",
-        required: true
-      },
-      # fixity
-      {
-        remote: @options[:fixity],
-        local: "#{aip_dirs.logs}/content_fixity_report.n3",
-        required: true
-      },
-      #content_datastream_metadata
-      {
-        remote: @options[:content_meta],
-        local: "#{aip_dirs.metadata}/content_fcr_metadata.n3",
-        required: true
-      },
-      #versions
-      {
-        remote: @options[:versions],
-        local: "#{aip_dirs.metadata}/content_versions.n3",
-        required: true
-      },
-      # optional files
-      # thumbnail
-      {
-        remote: @options[:thumbnail],
-        local: "#{aip_dirs.thumbnails}/thumbnail",
-        required: false
-      },
-      #characterization
-      {
-        remote: @options[:characterization],
-        local: "#{aip_dirs.logs}/content_characterization.n3",
-        required: false
-      }
-    ]
+  def aip_files(options)
 
-    @options[:content]&.each do | value |
+    # required files
+    # object_metadata
+    @aip_files.push(
+      { name: 'object_metadata',
+        remote: options[:object_metadata],
+        local: "#{aip_dirs.metadata}/object_metadata.n3",
+        required: true }
+    )
+
+    # fixity
+    @aip_files.push(
+      { name: 'fixity',
+        remote: options[:fixity],
+        local: "#{aip_dirs.logs}/content_fixity_report.n3",
+        required: true }
+    )
+
+    # content_datastream_metadata
+    @aip_files.push(
+      { name: 'content_datastream_metadata',
+        remote: options[:content_meta],
+        local: "#{aip_dirs.metadata}/content_fcr_metadata.n3",
+        required: true }
+    )
+
+    # versions
+    @aip_files.push(
+      { name: 'versions',
+        remote: options[:versions],
+        local: "#{aip_dirs.metadata}/content_versions.n3",
+        required: true }
+    )
+
+    # optional files
+    # thumbnail
+    @aip_files.push(
+      { name: 'thumbnail',
+        remote: options[:thumbnail],
+        local: "#{aip_dirs.thumbnails}/thumbnail",
+        required: false }
+    )
+
+    # characterization
+    @aip_files.push(
+      { name: 'characterization',
+        remote: options[:characterization],
+        local: "#{aip_dirs.logs}/content_characterization.n3",
+        required: false }
+    )
+
+    options[:content]&.each do | value |
         @aip_files.push(
-          remote: value,
-          local: "#{@aip_directory}/#{File.basename(@options[value])}",
-          required: true
+          { name: 'content',
+            remote: value,
+            local: "#{@aip_directory}/data/#{File.basename(value)}",
+            required: true }
         )
     end
 
-    @options[:permissions]&.each do | value |
+    options[:permissions]&.each do | value |
         @aip_files.push(
-          remote: value,
-          local: "#{aip_dirs.metadata}/#{File.basename(value)}",
-          required: false
+          { name: 'permissions',
+            remote: value,
+            local: "#{aip_dirs.metadata}/#{File.basename(value)}",
+            required: false }
         )
     end
   end
@@ -127,6 +142,6 @@ class LightAipCreation::AipCreator
   end
 
   def validate_files
-      @aip_files.each { |file| raise "missing file #{file[:remote]}" if file[:required] && file[:remot].nil? }
+      @aip_files.each { |file| raise "missing file type <#{file[:name]}> at location <#{file[:remote]}>" if file[:required] && (file[:remote].nil? || !File.exists?(file[:remote])) }
   end
 end
